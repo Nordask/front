cff9270d1a2bc3b40e9a3b47ed63a61c1eb71843
Unknown <solomon7dun@gmail.com>
2019-01-13 06:41:54 +0300

    Improved random quote machine: now it get quotes array by synchronous http request

--- workspace_js/projects/random-quote-machine/js/index.js
+++ workspace_js/projects/random-quote-machine/js/index.js
@@ -4,7 +4,7 @@ localStorage.setItem('example_project', 'Randowm Quote Machine');
 
 var colors = ['#7F6BC5', '#343043', '#1B53BE', '#1BBEA1', '#1BBE5A', '#A7DCBB', '#92E71D','#24360A', '#E1611B', '#8F4F2C', '#6F6E6E', '#E13157', '#964E5D', '#C919E7', '#691179', '#19041C']
 
-var quotes = [{quote:'Each event is preceded by prophecy. But, without the hero, there is no event.', author: 'Zurin Arctus'}, {quote:'quote1', author: 'author1'}, {quote:'quote2', author: 'author2'}, {quote:'quote3', author: 'author3'}, {quote:'quote4', author: 'author4'}]
+var quotes = []
 
 var quote = ''
 var author = ''
@@ -19,25 +19,26 @@ var getColor = () => {
   return colors[getRandomIntInRange(0, colors.length)]
 }
 
+function getQuotesContainerFromURL() {
+  let req = new XMLHttpRequest()
+  req.open("GET", 'https://raw.githubusercontent.com/Nordask/front/master/workspace_js/projects/random-quote-machine/quotes.json', false)
+  req.send(null)
+  if (req.status === 200) {
+    let json = JSON.parse(req.responseText)
+    quotes = json.quotes
+  }
+}
+
 function showQuote() {
-  //console.log(getQuote())
+  if(quotes.length === 0)
+    getQuotesContainerFromURL()
   let randomQuote = getQuote()
   quote = randomQuote.quote
   author = randomQuote.author
-  //console.log(quote + "\n" + author )
-  //text
   $('#forText').text(quote)
   $('#forAuthor').text("-" + author)
   
-  //background color
   let color = getColor()
-  /*
-  $("#main").animate(
-    {
-      backgroundColor: color
-    },
-    1000
-  )*/
  
   $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '"' + author))
   $('#main').css('background-color', color)
@@ -48,17 +49,7 @@ function showQuote() {
 $(document).ready(function() {
   showQuote()
   $("#new-quote").on('click', showQuote)
-  /*
-  if(!inFrame()) {
-  $("#tweet-quote").on('click', () => {
-    console.log("Tweet quote")
-     openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '"' + author))
-  });}*/
 })
-/*
-var inIframe = () => {
-  return window.self !== window.top
-}*/
 
 function openURL(url){
   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
